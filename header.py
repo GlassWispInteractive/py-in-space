23bcce5710c80aafd31f35ef0f6dfd054d9d2b7c
Lennart Buhl <git@hackmate.de>
2013-02-28 02:09:46 +0100

    small fixes, created draw_header

--- header.py
+++ header.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 *-*
-import pygame, sys
+import pygame
+import sys
 
-def draw_header(life, shield, bonus, pts):
-	
-	
-	
-	
-	
+
+def draw_header(surface, life, shield, bonus, pts):
+	header_sprites = dict()
+	for e in ['heart', 'shield', 'lightning', 'coins']:
+		header_sprites[e] = pygame.image.load('res/' + e + '.png')
+	surface.blit(header_sprites['heart'], (4, 4))
+	surface.blit(header_sprites['shield'], (84, 4))
+	surface.blit(header_sprites['lightning'], (168, 4))
+	surface.blit(header_sprites['coins'], (864, 4))
--- stars.py
+++ stars.py
@@ -1,39 +1,42 @@
-import pygame, sys
+import pygame
+import sys
 from pygame.locals import *
 from random import randint
 import math
 
 def draw_star(surface, (x,y), color, kind=0, scale=1):
-	position = lambda (a,b) : (int(a*scale+x), int(b*scale+y))
+	position = lambda (a,b): (int(a*scale+x), int(b*scale+y))
 	def darken((a,b,c),x): return (int(a/x), int(b/x), int(c/x))
-	
+
 	if kind == 0:
 		surface.set_at((x, y), color)
-	
+
 	elif kind == 1:
 		r = randint(1,4)
 		pygame.draw.circle(surface, color, (x, y), r)
-		
+
 	elif kind == 2:
 		"""handdrawn 'dot'"""
 		
 		colors = dict()
 		for i in range(1,4):
-			colors[i] = darken((color[0],color[1],color[2]), i)
-		
-		inner = [(2,1),(1,2),(2,2),(3,2),(2,3)]
+			colors[i] = darken((color[0], color[1], color[2]), i)
+
+		inner = [(2, 1), (1, 2), (2, 2), (3, 2), (2, 3)]
 		inner = list(map(position,inner))
-		for p in inner: surface.set_at(p, colors[1])
-		
+		for p in inner:
+			surface.set_at(p, colors[1])
+
 		middle = [(1,1),(3,1),(3,3),(1,3)]
 		middle = list(map(position,middle))
-		#for p in middle: surface.set_at(p, colors[2])
-		for p in middle: surface.set_at(p, colors[2])
+		for p in middle:
+			surface.set_at(p, colors[2])
 
 		outer = [(2,0),(4,2),(2,4),(0,2)]
 		outer = list(map(position,outer))
-		for p in outer: surface.set_at(p, colors[3])
-		
+		for p in outer:
+			surface.set_at(p, colors[3])
+
 	elif kind == 3:
 		"""the real polygon thing"""
 		
